b5893305c5a9197027e5f3b75c18c2805aae70f2
faeraga <faeraga@faec.me>
2019-12-27 13:39:50 -0500

    working on command line

--- src/command_line/mod.rs
+++ src/command_line/mod.rs
@@ -2,11 +2,87 @@ mod command_apex_set;
 
 use std::fmt;
 use std::error::Error;
+use clap::{Arg, App, SubCommand};
 //use std::boxed::Box;
 
+fn subcommand_apexset_create<'a, 'b>() -> App<'a, 'b> {
+  SubCommand::with_name("create")
+    .arg(Arg::with_name("name")
+      .index(1)
+      .required(true)
+      .help("The name of the new apex set.")
+    )
+    .arg(Arg::with_name("count")
+      .short("c")
+      .long("count")
+      .takes_value(true)
+      .required(true)
+      .validator(|count| {
+        count.parse::<u32>()
+          .map(|_| {})
+          .map_err(|_| "expected integer".to_string())
+      })
+      .help("The number of random apexes to generate.")
+    )
+    .arg(Arg::with_name("grid_density")
+      .short("g")
+      .long("grid_density")
+      .value_name("density")
+      .takes_value(true)
+      .validator(|density| {
+        density.parse::<u32>()
+          .map(|_| {})
+          .map_err(|_| "expected integer".to_string())
+      })
+      .help("The density of the generating grid, in log base 2.")
+    )
+    .arg(Arg::with_name("overwrite")
+      .short("o")
+      .long("overwrite")
+      .requires("name")
+      .help("Overwrite this set if it already exists.")
+    )       
+}
 
-pub fn run(args: &[String]) -> Result<()> {
-  println!("hello {:?}", args);
+fn subcommand_apexset_list<'a, 'b>() -> App<'a, 'b> {
+  SubCommand::with_name("list")
+}
+
+fn subcommand_apexset_delete<'a, 'b>() -> App<'a, 'b> {
+  SubCommand::with_name("delete")
+    .arg(Arg::with_name("name")
+      .index(1)
+      .required(true)
+      .help("The name of the apex set to delete.")
+    )
+}
+
+fn subcommand_apexset<'a, 'b>() -> App<'a, 'b> {
+  SubCommand::with_name("apexset")
+    .subcommands(vec![
+      subcommand_apexset_create(),
+      subcommand_apexset_list(),
+      subcommand_apexset_delete(),
+    ])
+}
+
+pub fn run() {
+  let matches = App::new("billiards-rs")
+    .version("0.0.x")
+    .subcommand(subcommand_apexset())
+    .get_matches();
+
+  if let Some(matches) = matches.subcommand_matches("apexset") {
+    println!("apexset");
+    if let Some(matches) = matches.subcommand_matches("create") {
+      println!("create");
+      let overwrite = matches.occurrences_of("overwrite");
+      println!("overwrite: {}", overwrite > 0);
+    }
+  }
+}
+
+pub fn run_old(args: &[String]) -> Result<()> {
   match args.first() {
     None => Err(CommandLineError::new(&format!("Expected command"))),
     Some(command) => {
@@ -33,8 +109,8 @@ mod tests {
 
   #[test]
   fn test_run() {
-    let result = run(&[]);
-    result.expect_err("run with empty arguments should yield an error");
+    //let result = run(&[]);
+    //result.expect_err("run with empty arguments should yield an error");
   }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-//extern crate billiards;
-
+#![allow(dead_code, unused_variables, unused_imports)]
 extern crate chrono;
+extern crate clap;
 //#[macro_use] extern crate maplit;
 extern crate rug;
 extern crate rand;
@@ -14,10 +14,11 @@ mod geometry;
 use std::env;
 
 fn main() {
-  let args: Vec<String> = env::args().collect();
+  /*let args: Vec<String> = env::args().collect();
   //println!("{:?}", args);
   let result = command_line::run(&args[1..]);
   if let Err(err) = result {
     println!("Error: {}", err)
-  }
+  }*/
+  command_line::run();
 }
